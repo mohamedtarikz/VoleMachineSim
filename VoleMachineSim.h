4b10077ed3d0b7a96012912f23421d050a23182a
mohamedtarikz <mohamedtarik06@gmail.com>
2024-10-23 14:30:45 +0300

    VoleMachineSim header - DONE

--- VoleMachineSim.h
+++ VoleMachineSim.h
@@ -1,7 +1,6 @@
 #ifndef VOLEMACHINESIM_VOLEMACHINESIM_H
 #define VOLEMACHINESIM_VOLEMACHINESIM_H
 
-#include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
@@ -38,20 +37,41 @@ public:
 
 // ALU class to handle arithmetic and logic operations
 class ALU {
+public:
     // Method to check if a string is a valid hexadecimal number
     bool isValid(string);
     // Method to convert a hexadecimal string to a decimal integer
-    int hexToDec(string);
+    static int hexToDec(string);
     // Method to convert a decimal integer to a hexadecimal string
-    string decToHex(int);
+    static string decToHex(int);
+    // Method to add to register values
+    int add(int idxRegister1, int idxRegister2, Register& reg);
+};
+
+// CU class to handle control operations
+class CU{
+public:
+    // Method to load a value from memory to a register
+    void load(int idxRegister, int idxMemory, Register& reg, Memory& mem);
+    // Method to load a value to a register
+    void load(int idxRegister, int value, Register& reg);
+    // Method to store a value from a register to memory
+    void store(int idxRegister, int idxMemory, Register& reg, Memory& mem);
+    // Method to move a value from one register to another
+    void move(int idxRegister1, int idxRegister2, Register& reg);
+    // Method to jump to a memory location
+    void jump(int idxRegister, int idxMemory, Register& reg, int& PC);
+    // Method to halt the program
+    void halt();
 };
 
 // CPU class to handle the execution of instructions
 class CPU {
-    int PC = 0; // Program Counter to keep track of the current instruction
+    int PC; // Program Counter to keep track of the current instruction
     string IR; // Instruction Register to store the current instruction
     Register reg; // Register object to handle register operations
     ALU alu; // ALU object to handle arithmetic and logic operations
+    CU cu; // CU object to handle control operations`
     // Method to fetch the next instruction from memory
     void fetch(Memory&);
     // Method to decode the fetched instruction
@@ -71,6 +91,10 @@ class VoleMachineSim {
     CPU cpu;
 public:
     VoleMachineSim();
+};
+
+class MainUI{
+
 };
 
 #endif //VOLEMACHINESIM_VOLEMACHINESIM_H
