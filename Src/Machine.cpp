4e3b8f3b86d7ecee844ae052765f2b9d50c98f15
mohamedtarikz <mohamedtarik06@gmail.com>
2024-10-25 22:24:57 +0300

    Machine - DONE

--- Src/Machine.cpp
+++ Src/Machine.cpp
@@ -5,21 +5,10 @@ Machine::Machine(fstream& instructions) : mem(instructions), cpu() {}
 
 Machine::Machine(vector<string> instructions) : mem(std::move(instructions)), cpu(){}
 
-void Machine::outputState(Register& reg) {
-    for (int i = 0; i < 16; ++i) {
-        cout<<"Address 0x"<<ALU::decToHex(i)<<": "<<ALU::decToHex(reg.getCell(i))<<endl;
-    }
+Register& Machine::getRegister() {
+    return cpu.getRegister();
 }
 
-void Machine::outputState(Memory& memory) {
-    for (int i = 0; i < 256; i+=2) {
-        cout<<"Address 0x"<<ALU::decToHex(i)<<": "<<memory.getCell(i)<<memory.getCell(i+1)<<endl;
-    }
-}
-
-void Machine::outputState(Register& reg, Memory& memory) {
-    cout<<"Register state:"<<endl;
-    outputState(reg);
-    cout<<"Memory state:"<<endl;
-    outputState(memory);
+Memory& Machine::getMemory() {
+    return mem;
 }
